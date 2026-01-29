b5155dfcb7834b144ec29f529994b439c5842cbe
Oloo <ndedastacy@gmail.com>
2026-01-29 20:39:03 +0300

    initial comm

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const auth = require("./middleware/auth.middleware");
+
+require("dotenv").config();
+
+
+const app = express();
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/auth", require("./routes/auth.routes"));
+
+app.get("/api/protected", auth, (req, res) => {
+  res.json({
+    message: "You accessed a protected route!",
+    userId: req.user.id
+  });
+});
+
+
+app.get("/", (req, res) => {
+  res.send("E-Commerce API is running...");
+});
+
+
+
+app.listen(process.env.PORT, () => console.log("Server running on port 5000"));
